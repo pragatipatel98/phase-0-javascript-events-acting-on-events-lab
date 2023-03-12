0f568893790c930ee71a6faa33eaab9dd0facd53
Pragati Patel <pragatinpatel@gmail.com>
2023-03-12 10:18:13 -0400

    lab complete

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Your code here
+
+document.addEventListener("keydown", function (e) {
+    if (e.key === "ArrowLeft") {
+        moveDodgerLeft();
+    } else if (e.key === "ArrowRight") {
+        moveDodgerRight();
+    }
+});
+
+function moveDodgerLeft() {
+    let dodger = document.getElementById("dodger");
+    let left = parseInt(dodger.style.left.replace("px", ""));
+
+    if (left > 0) {
+        dodger.style.left = `${left - 1}px`;
+    }
+}
+
+function moveDodgerRight() {
+    let dodger = document.getElementById("dodger");
+    let left = parseInt(dodger.style.left.replace("px", ""));
+
+    if (left < 360) {
+        dodger.style.left = `${left + 1}px`;
+    }
+}
